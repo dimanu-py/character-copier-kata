7fbd6d153ad296bc9d5e1accc7a32e7413663b1a
dimanu-py <diegomtz126@gmail.com>
2024-06-04 17:49:04 +0200

    test(source): verify StringReader can read a word character by character

--- tests/test_source.py
+++ tests/test_source.py
@@ -15,3 +15,15 @@ class TestStringReader:
         read_char = reader.get_char()
 
         assert read_char == expected_char
+
+    @pytest.mark.parametrize(
+        ["input_string", "expected_chars"],
+        [("abc", ["a", "b", "c", "\n"]), ("defghi", ["d", "e", "f", "g", "h", "i", "\n"])]
+    )
+    def test_given_a_word_should_return_separated_chars(self, input_string, expected_chars):
+        reader = StringReader(string_to_read=input_string)
+        iterations = len(input_string) + 1
+
+        read_chars = [reader.get_char() for _ in range(iterations)]
+
+        assert read_chars == expected_chars
